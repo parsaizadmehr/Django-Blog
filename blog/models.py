cfd65bdf9305ea08e5521f7cbc09f4640f7674f3
Parsa <112335030+parsaizadmehr@users.noreply.github.com>
2023-11-15 23:10:59 +0330

    Models added.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    STATUS_CHOICES = [
+        ("d", "Draft"),
+        ("p", "Published"),
+    ]
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=100, unique=True)
+    content = models.TextField()
+    thumbnail = models.ImageField(upload_to="uploads")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}"
